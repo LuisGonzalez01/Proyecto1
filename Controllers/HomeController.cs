104d03c96f307805d8168f231b07fe1101dd2b6e
LuisGonzalez996 <elmorenito0880@gmail.com>
2019-11-17 11:40:39 -0400

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "<html><body>" +
+                "<h1>Universidad Autonoma de Santo Domingo(UASD)</h1>" +
+                "<P>Diplomado Desarrolo Web C#, MVC </P>" +
+                "<body></html>";
+        }
+        public string Diplomado() 
+        {
+            return "<html><body>" +
+                "<h1>Estudiantes:</h1>" +
+                "<p>Yorqui Montero Sanchez<br>" +
+                "Junior Maria Araujo<br>" +
+                "</body><html>";
+        }
+    }
+}
